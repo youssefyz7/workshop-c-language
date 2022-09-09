66873e9dfe14945f82512e357298f12a9c22ce40
youssefyz7 <yousseframzaoui@gmail.com>
2022-09-09 09:47:26 +0100

    Challenge 13 : Affichage de la valeur octale et hexadécimale équivalente

---
+++ challenge13.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+#include<math.h>
+
+int main(){
+  //variable :
+  int  d;
+
+  printf("saisir un nombre decimal : ");
+  scanf("%d",&d);
+
+  //traitment et resultat :
+
+  printf(" octal = %o",d);
+  printf(" hexadécimal = %x",d);
+
+	return 0;
+}
